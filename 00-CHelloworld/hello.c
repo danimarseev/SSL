c2ec1e063395f5f75a4567bbe8b655ac22ec7ace
danimarseev <dmarseev@frba.utn.edu.ar>
2023-05-04 08:40:28 -0300

    agrega saludo con salida a un txt

--- 00-CHelloworld/hello.c
+++ 00-CHelloworld/hello.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 int main() {
-    fprintf(stdout,"Hello, World!\n");
-    //Manera más simplificada de mostrar por consola
-    //printf(stdout,"Hello, World!\n") 
+    FILE *salida;
+    salida = fopen("output.txt","w"); //usa el parámetro "w" para reescribir
+    
+    fprintf(stdout,"Hello, World!\n"); //imprime por consola
+    fprintf(salida,"Hello, World!\n"); //guarda en output.txt
+
+    fclose(salida);
+    
     return 0;
 }
